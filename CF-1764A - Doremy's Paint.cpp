e0b59863846722367773540f741e1cd041bd9b79
gonap <ajaygonepuri@gmail.com>
2022-12-31 21:35:20 +0530

    Codeforces

---
+++ CF-1764A - Doremy's Paint.cpp
@@ -0,0 +1,21 @@
+// Print all possible DFS paths for a acyclic graph
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            v.push_back(a);
+        }
+        cout<<1<<" "<<n<<endl;
+    }
+}
